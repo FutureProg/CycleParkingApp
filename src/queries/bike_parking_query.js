2bb4afd3514d394cd279c81d813c37e3ed7dfd9f
Nick Morrison <nickmorrison09@gmail.com>
2024-05-11 13:21:40 -0400

    Changed query to use Halton Region instead of Mississauga

--- src/queries/bike_parking_query.js
+++ src/queries/bike_parking_query.js
@@ -3,7 +3,7 @@ export default `
 [out:json]
 [timeout:25]
 ;
-area(3601954127)->.searchArea;
+area(3607034889)->.searchArea;
 (
   node
     ["bicycle_parking"]
@@ -19,3 +19,9 @@ out;
 >;
 out skel qt;
 `
+
+// Oakville - 2407500
+// Burlington - 2407513 
+// Milton - 2414122
+// Halton Hills - 2414222
+// Halton Region - 7034889
